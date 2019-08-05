e236a5e5a06eecd83a6567e962c9de738c6e261c
David Pérez <aeterno1@hotmail.com>
2019-08-05 10:20:02 +0200

    Primer "Hola Mundo" en java, inicial

---
+++ src/Source.java
@@ -0,0 +1,30 @@
+
+public class Source {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Primer programa de prueba que muestra en la consola un "Hola Mundo"
+		
+		// Este es un comentario simple, se usan "//"
+		
+		/* Este es un comentario con varias líneas, se usa la barra y el asterisco
+		 * esta sería la manera idónea, abrir con "/*"
+		 * y se cierra con "* /"
+		 */
+		
+		// con print imprime en una línea sin hacer retorno de carro
+		System.out.print("Hola Mundo!");
+		// \n es un salto de línea, en este ejemplo lo ponemos manualmente
+		System.out.print("Esta es la segunda linea\n");
+		// \\n imprime por pantalla el \n al no considerarlo como caracter especial
+		System.out.print("Mostramos el \\n");
+		
+		// Realizamos dos saltos de línea.
+		System.out.print("\n\n");
+		
+		// con println imprime en una linea nueva 
+		System.out.println("Hola Mundo!");
+		System.out.println("Esta es la segunda linea");
+	}
+
+}
